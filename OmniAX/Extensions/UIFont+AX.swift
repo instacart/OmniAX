7277ffaa5338c042e2464fb4957708a58b305015
Dan L <daniel.h.loman@gmail.com>
2017-10-23 17:37:33 -0700

    Initial commit to support custom font scaling

--- OmniAX/Extensions/UIFont+AX.swift
+++ OmniAX/Extensions/UIFont+AX.swift
@@ -7,3 +7,56 @@
 //
 
 import Foundation
+
+extension UIFont {
+    public func scaled(style: UIFontTextStyle = .headline) -> UIFont {
+        let attributes: [String: Any] = [
+            UIFontDescriptorSizeAttribute: scaled(fontSize: pointSize, style: .headline)
+        ]
+
+        return UIFont(descriptor: fontDescriptor.addingAttributes(attributes), size: 0)
+    }
+
+    // Default to headline style to prevent massive sizes from making unoptimized apps unusable
+    func scaled(fontSize: CGFloat, style: UIFontTextStyle = .headline) -> CGFloat {
+        let systemSize = UIFontDescriptor.preferredFontDescriptor(withTextStyle: style).pointSize
+
+        guard systemSize != style.defaultSize else {
+            return fontSize
+        }
+
+        return ceil((systemSize / style.defaultSize) * fontSize)
+    }
+ }
+
+private extension UIFontTextStyle {
+    var defaultSize: CGFloat {
+        switch self {
+        case .body:
+            return 17
+        case .callout:
+            return 16
+        case .caption1:
+            return 12
+        case .caption2:
+            return 10
+        case .footnote:
+            return 13
+        case .headline:
+            return 17
+        case .subheadline:
+            return 15
+        case .title1:
+            return 28
+        case .title2:
+            return 22
+        case .title3:
+            return 20
+        default:
+            if #available(iOS 11.0, *), self == .largeTitle {
+                return 34
+            }
+            return 17
+        }
+    }
+}
